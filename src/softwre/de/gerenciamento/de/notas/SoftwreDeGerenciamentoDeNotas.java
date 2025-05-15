2b663b80b38db9cc1c3e275dbc0766cadd5cd5ae
Vitor Gabriel <vitor.g.lisboa2018@gmail.com>
2025-05-15 19:02:59 -0300

    metado criar login

--- src/softwre/de/gerenciamento/de/notas/SoftwreDeGerenciamentoDeNotas.java
+++ src/softwre/de/gerenciamento/de/notas/SoftwreDeGerenciamentoDeNotas.java
@@ -5,6 +5,15 @@ public class SoftwreDeGerenciamentoDeNotas {
 
     
     public static void main(String[] args) {
+        
+        login cadastro = new login();
+        cadastro.criarLogin();
+        //cadastro.fazerLogin();
+      
+        
+        
+        
+        
       
     }
     
--- src/softwre/de/gerenciamento/de/notas/login.java
+++ src/softwre/de/gerenciamento/de/notas/login.java
@@ -3,17 +3,46 @@ package softwre.de.gerenciamento.de.notas;
 import java.util.Scanner;
 
 public class login {
+
+    String senha = "";
+    void criarLogin() {
+        
+        Scanner cadastroEmail = new Scanner(System.in);
+        System.out.print("Vamos fazer seu cadastro  \nDigite seu e-mail ");
+        String email = cadastroEmail.nextLine();    
+        
+        
+        Scanner cadastroSenha = new Scanner(System.in);
+        System.out.print("CRIE sua senha ");
+        String senha = cadastroSenha.nextLine();
     
+        
+    }
     
-    public static void criarLogin() {
-    Scanner senha = new Scanner(System.in);
-    System.out.print("CRIE sua senha ");
-    String cadastroSenha = senha.nextLine();
-    
-    
-    Scanner Email = new Scanner(System.in);
-    System.out.print("Digite seu e-mail ");
-    String cadastroEmail = senha.nextLine();
+    void fazerLogin(){
+        Scanner usuario = new Scanner(System.in);
+        System.out.print("Digite seu e-mail ");
+        String loginSenha = usuario.nextLine();
+        
+        System.out.print(loginSenha );
+        
+        boolean vld = loginSenha.equals(senha);
+         System.out.print(senha );
+
+        
+        if(vld){
+          
+        System.out.print(loginSenha);
+
+        
+        }else{
+        System.out.print("errado");
+        
+        }
+        
+        
+        
+        
         
     }
     
